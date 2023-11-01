7c5f31143cef4876c41f0eaa58c6ef62b0fc127d
vivek <ramrao.repale@albanero.io>
2023-11-01 16:57:38 +0530

    Added json server and icon

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
 // import { Outlet } from "react-router-dom"
-import { Link } from 'react-router-dom';
-import { Drawer,List,ListItem,ListItemIcon,ListItemText } from '@material-ui/core';
+import { Link } from "react-router-dom";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Icon,
+} from "@material-ui/core";
 // import {HomeIcon,DashboardIcon,SettingsIcon} from ''
 // import Drawer from '@mui/material/Drawer';
 // import List from '@mui/material/List';
@@ -11,41 +18,36 @@ import { Drawer,List,ListItem,ListItemIcon,ListItemText } from '@material-ui/cor
 // import HomeIcon from '@mui/icons-material/Home';
 // import DashboardIcon from '@mui/icons-material/Dashboard';
 // import SettingsIcon from '@mui/icons-material/Settings';
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: "flex" }}>
       <Drawer variant="permanent">
         <List>
           <ListItem button component={Link} to="/users">
             <ListItemIcon>
               {/* <HomeIcon /> */}
+              <Icon>save</Icon>
             </ListItemIcon>
             <ListItemText primary="Users" />
           </ListItem>
           <ListItem button component={Link} to="/authorization">
-            <ListItemIcon>
-              {/* <DashboardIcon /> */}
-            </ListItemIcon>
+            <ListItemIcon>{/* <DashboardIcon /> */}</ListItemIcon>
             <ListItemText primary="Permissions" />
           </ListItem>
           <ListItem button component={Link} to="/csv">
-            <ListItemIcon>
-              {/* <DashboardIcon /> */}
-            </ListItemIcon>
+            <ListItemIcon>{/* <DashboardIcon /> */}</ListItemIcon>
             <ListItemText primary="Csv-Upload" />
           </ListItem>
           <ListItem button component={Link} to="/game">
-            <ListItemIcon>
-              {/* <SettingsIcon /> */}
-            </ListItemIcon>
+            <ListItemIcon>{/* <SettingsIcon /> */}</ListItemIcon>
             <ListItemText primary="TicTacToe" />
           </ListItem>
         </List>
       </Drawer>
-      <div style={{ marginLeft: '240px' }}>
+      <div style={{ marginLeft: "240px" }}>
         {/* <Outlet /> */}
         {children}
-        </div>
+      </div>
     </div>
   );
 };
