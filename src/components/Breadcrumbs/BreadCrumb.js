276aee25cf00c616d697958899af484e4bcf4f3e
vivek <ramrao.repale@albanero.io>
2023-11-06 09:31:02 +0530

    Fixed UI

--- src/components/Breadcrumbs/BreadCrumb.js
+++ src/components/Breadcrumbs/BreadCrumb.js
@@ -1,30 +1,26 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom/cjs/react-router-dom";
-import { Breadcrumbs,Typography } from "@material-ui/core";
+import { Link, useLocation } from "react-router-dom";
+import { Breadcrumbs, Typography } from "@material-ui/core";
 
-function BreadCrumb (){
-    const location = useLocation();
-    const pathName = location.pathname.split("/").filter((path)=>path);
-    return(
-       <Breadcrumbs>
-         {pathName.map((path,index)=>{
-            const end = index === pathName.length-1;
-            const to = `/${pathName.slice(0,index+1).join("/")}`;
+function BreadCrumb() {
+  const location = useLocation();
+  const pathName = location.pathname.split("/").filter((path) => path);
+  return (
+    <Breadcrumbs>
+      {pathName.map((path, index) => {
+        const end = index === pathName.length - 1;
+        const to = `/${pathName.slice(0, index + 1).join("/")}`;
 
-            return(
-                (end)? (
-                    <Typography key={to}>
-                        {path}
-                    </Typography>
-                ) : (
-                    <Link key={to} to={to}>
-                        {path}
-                    </Link>
-                )
-            )
-         })}
-       </Breadcrumbs>
-    )
+        return end ? (
+          <Typography key={to}>{path}</Typography>
+        ) : (
+          <Link key={to} to={to}>
+            {path}
+          </Link>
+        );
+      })}
+    </Breadcrumbs>
+  );
 }
 
 export default BreadCrumb;
