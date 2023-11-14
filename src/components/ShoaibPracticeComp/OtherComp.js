0951fb2e50d72dcd63d8b2dcbb3571c0c10a97ca
shoaib20-1998 <sm920149@gmail.com>
2023-11-14 09:41:00 +0530

    done

---
+++ src/components/ShoaibPracticeComp/OtherComp.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import * as list from "@platform/service-ui-libraries";
+import { AlbaButton } from "@platform/service-ui-libraries";
+
+function OtherComp() {
+  console.log(list);
+
+  const StatrtButton = () => {
+    return <AlbaButton>hy</AlbaButton>;
+  };
+
+  useEffect
+  return <div>hy</div>;
+}
+
+export default OtherComp;
